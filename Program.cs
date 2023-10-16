41d9368c4f934416b4d31f3b2d69aa78126f7674
mateus <imw.desenv@gmail.com>
2023-10-16 17:46:47 -0300

    Save credentials

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Pulumi;
+﻿using System;
+using Pulumi;
 using Docker = Pulumi.Docker;
 
 return await Deployment.RunAsync(() =>
@@ -8,10 +9,13 @@ return await Deployment.RunAsync(() =>
    var rabbitMQImageName = configs.Require("rabbitMQ-image");
    var msHistoryImage = configs.Require("ms-history-image");
 
+   var dbUser = configs.Require("dbUser");
+   var dbPassword = configs.RequireSecret("dbPassword");
+
    var network = new Docker.Network("network-univan");
 
    CreateRabbitMQContainer(rabbitMQImageName, network);
-   CreateHistoryMicroservice(msHistoryImage, network);
+   CreateHistoryMicroservice(msHistoryImage, network, dbUser,   dbPassword);
 });
 
 
@@ -44,7 +48,7 @@ static void CreateRabbitMQContainer(string rabbitMQImageName, Docker.Network net
    });
 }
 
-static void CreateHistoryMicroservice(string msHistoryImage, Docker.Network network)
+static void CreateHistoryMicroservice(string msHistoryImage, Docker.Network network, string dbUser, Output<string> dbPassword)
 {
    var msHistory = StackHelper.GetDockerImage(msHistoryImage, "univan-history");
 
@@ -60,7 +64,7 @@ static void CreateHistoryMicroservice(string msHistoryImage, Docker.Network netw
          }
       },
       Envs = new InputList<string> {
-         $"ConnectionStrings__HistoryDatabase=Server=tcp:tccunivanfinal.database.windows.net,1433;Initial Catalog=history;Persist Security Info=False;User ID=tccunivan;Password=Project!123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+         dbPassword.Apply(password => $"ConnectionStrings__HistoryDatabase=Server=tcp:tccunivanfinal.database.windows.net,1433;Initial Catalog=history;Persist Security Info=False;User ID={dbUser};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"),
          $"ConnectionStrings__RabbitMq=amqp://guest:guest@rabbit-service:5672"
       },
       NetworksAdvanced = new InputList<Docker.Inputs.ContainerNetworksAdvancedArgs> {
